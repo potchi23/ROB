481fd1e9fa68f8b34ef0e29e5dc78b27c5172836
potchi23 <53357059+potchi23@users.noreply.github.com>
2020-12-12 12:04:12 +0100

    Revert "no funciona"
    This reverts commit 57ccb90c22f6ddeba82bef4ba8ca90b1056520bb.

--- grafo.h
+++ grafo.h
@@ -23,9 +23,7 @@ class Grafo{
         DiccionarioHash<T, Lista<T>> nodos;
         unsigned int _nNodos;
         unsigned int _nAristas;
-        void recorreLista(const T &ant, const T &origen, const T &act, const T &destino, bool flags[], int &n);
-        
-        Lista<T> d;
+        void recorreLista(const T &ant, const T &origen, const T &act, const T &destino, Lista<T> &lista, bool flags[], int &n) const;
 
     public: 
         Grafo(): _nNodos(0), _nAristas(0){};
@@ -34,7 +32,7 @@ class Grafo{
         void uneNodos(const T &nodo1, const T &nodo2, const int &peso);
         void representa() const;
         unsigned int numNodos() const;
-        void muestraCaminos(const T &origen, const T&destino);
+        void muestraCaminos(const T &origen, const T&destino, Lista<int> &lista, bool flags[]) const;
         void dijkstra(const T &origen, const T&destino) const;
 };
 
